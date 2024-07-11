f4151e7ce2ed23cd5cd5a720077cb0e7969df427
Rique22 <108375075+Rique22@users.noreply.github.com>
2024-07-10 22:19:31 -0300

    Adicionando try-catch

--- src/router.js
+++ src/router.js
@@ -12,32 +12,52 @@ router.use(express.json());//permission to use json
 
 //select all books
 router.get('/livros', async (req, res) => {
-    const books = await db.selectionBooks();
-    res.status(200).json(books);
+    try {
+        const books = await db.selectionBooks();
+        res.status(200).json(books);
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha ao selecionar livro` });
+    }
 });
 
 //select a book
 router.get('/livros/:id', async (req, res) => {
-    const book = await db.selectionBook(req.params.id);
-    res.status(200).json(book);
+    try {
+        const book = await db.selectionBook(req.params.id);
+        res.status(200).json(book);
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha ao selecionar livro` });
+    }
 });
 
 //insert a book
 router.post('/livros', async (req, res) => {
-    await db.insertBook(req.body);
-    res.status(201).send('Livro cadastrado com sucesso');
+    try {
+        await db.insertBook(req.body);
+        res.status(201).send('Livro cadastrado com sucesso');
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha ao selecionar livro` });
+    }
 });
 
 //update a book
 router.put('/livros/:id', async (req, res) => {
-    await db.updateBook(req.params.id, req.body);
-    res.status(200).send('Livro atualizado com sucesso');
+    try {
+        await db.updateBook(req.params.id, req.body);
+        res.status(200).send('Livro atualizado com sucesso');
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha ao selecionar livro` });
+    }
 });
 
 //delete a book
 router.delete('/livros/:id', async (req, res) => {
-    await db.deleteBook(req.params.id);
-    res.status(200).send('Livro removido com sucesso');
+    try {
+        await db.deleteBook(req.params.id);
+        res.status(200).send('Livro removido com sucesso');
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha ao selecionar livro` });
+    }
 });
 
 //exporting routers
